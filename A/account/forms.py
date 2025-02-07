b36719b53ca2211e96be9605febc3bd43fabf6f2
behrouzsh <behrouzsharifi7@gmail.com>
2025-02-07 22:51:40 +0330

    Preventing users from using an existing email address

--- A/account/forms.py
+++ A/account/forms.py
@@ -1,22 +1,18 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from .models import Profile
+from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(
-        label= 'password',
-        widget= forms.PasswordInput
-    )
-    password2 = forms.CharField(
-        label='Repeat password',
-        widget= forms.PasswordInput
-    )
+    password = forms.CharField(label= 'password', widget= forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget= forms.PasswordInput)
+
     class Meta:
-        model = get_user_model()
+        model = User 
         fields = ['username', 'first_name', 'email']
     
     def clean_password2(self):
@@ -25,10 +21,27 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords do not match')
         return cd['password2']
     
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError("Email already exists")
+        return data
+
+    
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['first_name', 'last_name', 'email']
+    
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        # Check if the email is the same as the current one
+        if data == self.instance.email:
+            raise forms.ValidationError("This is your current email.")
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError("Email already exists")
+        return data
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
