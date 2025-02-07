1e9561b7f34d3e3e7bf9b0f34be6d979d7b83295
behrouzsh <behrouzsharifi7@gmail.com>
2025-02-07 23:25:37 +0330

    Adding social authentication to your site

--- A/A/urls.py
+++ A/A/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social'))
 
 ]
 
@@ -29,3 +30,5 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+
+
